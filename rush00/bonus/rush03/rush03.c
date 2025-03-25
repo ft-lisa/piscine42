ef9e357ba9d7c408670cc8def39763ce3e3c1999
lisa <lisa.martin.lm02@gmail.com>
2025-03-25 19:31:15 +0100

    116%

---
+++ rush00/bonus/rush03/rush03.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbona <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/17 15:58:53 by pbona             #+#    #+#             */
+/*   Updated: 2024/08/18 18:56:00 by pbona            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_top_line(int j, int x)
+{
+	if (j == 0)
+		ft_putchar('A');
+	else if (j != x - 1)
+		ft_putchar('B');
+	else
+		ft_putchar('C');
+}
+/*1st: left, 2nd: middle, 3rd: right*/
+
+void	ft_middle_lines(int j, int x)
+{
+	if (j == 0 || j == x - 1)
+		ft_putchar('B');
+	else
+		ft_putchar(' ');
+}
+/*1st: left and right, 2nd: middle*/
+
+void	ft_bottom_line(int j, int x)
+{
+	if (j == 0)
+		ft_putchar('A');
+	else if (j != x - 1)
+		ft_putchar('B');
+	else
+		ft_putchar('C');
+}
+/*1st: left, 2nd: middle, 3rd: right*/
+
+void	rush(int x, int y)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i < y && x > 0)
+	{
+		j = 0;
+		while (j < x)
+		{
+			if (i == 0)
+				ft_top_line(j, x);
+			else if (i != y -1)
+				ft_middle_lines(j, x);
+			else
+				ft_bottom_line(j, x);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
